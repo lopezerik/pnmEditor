4a0680e21aa6b948e1dbf800f7590aa7c51c6c5e
lopezerik <eriklopezraya@gmail.com>
2020-02-08 18:16:27 -0800

    test commit

--- filters.C
+++ filters.C
@@ -6,28 +6,28 @@
 #include <logging.h>
 
 void Filter::Update(void){
-	if(img1 != NULL){
-		char msg[128];
-		sprintf(msg, "%s: about to update input1", FilterName());
-		Logger::LogEvent(msg);
-		img1->Update();
-		sprintf(msg, "%s: done updating input1", FilterName());
-		Logger::LogEvent(msg);
+    if(img1 != NULL){
+        char msg[128];
+        sprintf(msg, "%s: about to update input1", FilterName());
+        Logger::LogEvent(msg);
+        img1->Update();
+        sprintf(msg, "%s: done updating input1", FilterName());
+        Logger::LogEvent(msg);
 
-		if(img2 != NULL){
-			char msg[128];
-			sprintf(msg, "%s: about to update input2", FilterName());
-			Logger::LogEvent(msg);
-			img2->Update();
-			sprintf(msg, "%s: done updating input2", FilterName());
-			Logger::LogEvent(msg);
-		}
+        if(img2 != NULL){
+            char msg[128];
+            sprintf(msg, "%s: about to update input2", FilterName());
+            Logger::LogEvent(msg);
+            img2->Update();
+            sprintf(msg, "%s: done updating input2", FilterName());
+            Logger::LogEvent(msg);
+        }
 
-		sprintf(msg, "%s: about to execute", FilterName());
-		Logger::LogEvent(msg);
-		Execute();
-		sprintf(msg, "%s: done executing", FilterName());
-		Logger::LogEvent(msg);
+        sprintf(msg, "%s: about to execute", FilterName());
+        Logger::LogEvent(msg);
+        Execute();
+        sprintf(msg, "%s: done executing", FilterName());
+        Logger::LogEvent(msg);
 	}
 }
 
